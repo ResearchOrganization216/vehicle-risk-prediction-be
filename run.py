d4e1dbefcb68e90d5c886852536fc0771dcf8892
IT21259302-Narasinghe <nisalnarasinghe@gmail.com>
2025-03-17 22:31:56 +0530

    port change

--- run.py
+++ run.py
@@ -16,4 +16,4 @@ from flask_sqlalchemy import SQLAlchemy
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5005, debug=True)
