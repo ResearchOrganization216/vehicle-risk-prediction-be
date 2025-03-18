2e868ee5f8f581b52d05b15323763da5057d741c
IT21259302-Narasinghe <nisalnarasinghe@gmail.com>
2025-03-18 19:36:25 +0530

    deployment

--- app/config.py
+++ app/config.py
@@ -17,7 +17,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@34.142.175.163:5432/InnoAInsure'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BUCKET_NAME = "innoainsure-bucket"
-    KEY_PATH = "C:\\Users\\Sithija\\Documents\\keyfiles\\innoainsure-project-531bfaa81104.json"  # Set to an empty string in production
+    KEY_PATH = ""  # Set to an empty string in production
     PROJECT_ID = "innoainsure-project"
     SECRET_KEY = 'your-very-secret-key'
 
